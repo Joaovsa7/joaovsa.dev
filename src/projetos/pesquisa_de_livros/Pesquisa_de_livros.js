337d29a5bb0dd046bafc18356b879606b58860e9
joaovsa7 <jvsadesign@gmail.com>
2019-04-14 13:44:32 -0300

    adicionando a barra de navegacao no projeto pesquisa de livros

--- src/projetos/pesquisa_de_livros/Pesquisa_de_livros.js
+++ src/projetos/pesquisa_de_livros/Pesquisa_de_livros.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import Livro from './componentes/Livro';
+import Nav from './componentes/NavBar';
 
 const Pesquisa_de_Livros = () => {
     const [valor, setValor] = useState('')
@@ -23,6 +24,7 @@ const Pesquisa_de_Livros = () => {
 
     return ( 
         <div id="book_container">
+            <Nav />
             <div id="pesquisa">
                 <input type="text" onChange={(e) => setValor(e.target.value)} value={valor} placeholder="Pesquise um livro no Google Books" />
                 <button type="submit" onClick={() => setUrl(`https://www.googleapis.com/books/v1/volumes?q=${valor}`)}>Pesquisar</button>
